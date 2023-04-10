144d11894724ecf418a97d345e30e74259c1ed9b
Hà Minh Đức <duchm@ommanisoft.com>
2023-04-10 11:46:37 +0700

    d

---
+++ index.js
@@ -0,0 +1,19 @@
+function createRain() {
+  const rainContainer = document.querySelector(".rain-container");
+  const raindrop = document.createElement("div");
+  raindrop.classList.add("raindrop");
+
+  const startLeft = Math.random() * window.innerWidth;
+  const endLeft = startLeft + 10;
+  const duration = Math.random() * 4 + 1;
+
+  raindrop.style.left = `${startLeft}px`;
+  raindrop.style.animationDuration = `${duration}s`;
+
+  setTimeout(() => {
+    rainContainer.removeChild(raindrop);
+  }, duration * 1000);
+
+  rainContainer.appendChild(raindrop);
+}
+setInterval(createRain, 100);
